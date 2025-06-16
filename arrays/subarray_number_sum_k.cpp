32f04cd2a8fb4fbf7bf71bdbea92ce29680dadbd
arkak24 <arkak.brj@gmail.com>
2025-06-16 22:15:55 +0530

    number of subarrays

--- arrays/subarray_number_sum_k.cpp
+++ arrays/subarray_number_sum_k.cpp
@@ -1,6 +1,8 @@
 // have to find the number of subarray present in an array with sum k
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -40,15 +42,25 @@ using namespace std;
 // }
 
 int number_of_subarrays(vector<int> vect, long long k){
-      
+      std::unordered_map<int, int> mpp;
+      mpp[0] = 1;
+      int preSum = 0;
+      int count = 0;
+      for(int i = 0; i < vect.size(); i++){
+            preSum += vect[i];
+            int remove = preSum - k;
+            count = count + mpp[remove];
+            mpp[preSum] += 1;
+      }
+      return count;
 }
 
 int main(){
-      vector<int> vect = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-      int k = 5;
+      std::vector<int> vect = {1, 2, -3, 3, 1, 1, 1, 4, 2, -3};
+      int k = 3;
 
       int ans = number_of_subarrays(vect, k);
-      cout << ans << endl;
+      std::cout << ans << endl;
 
       return 0;
 }
