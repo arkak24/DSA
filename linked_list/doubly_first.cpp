33ea82f8a9e2a9c143ef8df0942c78caba5dc387
arkak24 <arkak.brj@gmail.com>
2025-08-27 00:25:46 +0530

    DLL

---
+++ linked_list/doubly_first.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+class Node{
+      public:
+            int data;
+            Node* next;
+            Node* back;
+
+            Node(int data1){
+                  data = data1;
+                  next = nullptr;
+                  back = nullptr;
+            }
+
+            Node(int data1, Node* next1, Node* back1){
+                  data = data1;
+                  next = next1;
+                  back = back1;
+            
+      }
+};
+
+Node* convertArr2DLL(std::vector<int> &arr){
+      Node* head = new Node(arr[0]);
+      Node* prev = head;
+
+      for(int i = 1; i < arr.size(); i++){
+            Node* temp = new Node(arr[i], nullptr, prev);
+            prev -> next = temp;
+            prev = temp;
+      }
+
+      return head;
+}
+
+void print_DLL(Node* head){
+      Node* temp = head;
+      while(temp != nullptr){
+            std::cout << temp -> data << "\t";
+            temp = temp -> next;
+      }
+      std::cout << "\n";
+}
+
+void free_DLL(Node* head){
+      Node* temp = nullptr;
+      while(head != nullptr){
+            temp = head -> next;
+            delete head;
+            head = temp;
+      }
+}
+
+int main(){
+      std::vector<int> vect = {1, 2, 3, 4, 5, 6};
+
+      Node* head = convertArr2DLL(vect);
+
+      print_DLL(head);
+
+      free_DLL(head);
+
+      return 0;
+}
